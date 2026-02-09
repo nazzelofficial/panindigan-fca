130cb1bfa0f92fdc56a4b2e97c4a8b5a22e30478
nazzeldevelopment <148680292+nazzeldevelopment@users.noreply.github.com>
2026-02-10 07:51:12 +0800

    Update

--- dist/command/CooldownManager.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CooldownManager {
-    private cooldowns;
-    constructor();
-    setCooldown(commandName: string, userId: string, durationSeconds: number): void;
-    checkCooldown(commandName: string, userId: string): number | null;
-    clearCooldown(commandName: string, userId: string): void;
-}
